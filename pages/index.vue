dcb0c0d13b9163253585c0bc406df4bb042d3ee9
inephillim <118970251+inephillim@users.noreply.github.com>
2022-12-31 02:41:39 -0800

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>This a simple api using app by mohammad zarei</p>
+    <p>Click on merch to see products</p>
+  </div>
+</template>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
+</style>
